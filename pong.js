17afd8df0c22b28ed7b29ebf9d94f21cb1725b72
Jose Rios <joseriosdev@gmail.com>
2022-01-18 13:10:05 -0500

    3.2 game finished

--- pong.js
+++ pong.js
@@ -3,20 +3,23 @@ var ctx;
 
 var bolaX = 50;
 var bolaY = 50;
-var velocidadBolaX = 10;
-var velocidadBolaY = 5;
+const VELOCIDAD_INICIAL_X = 10;
+const VELOCIDAD_INICIAL_Y = 5;
+var velocidadBolaX = VELOCIDAD_INICIAL_X;
+var velocidadBolaY = VELOCIDAD_INICIAL_Y;
 
 var raqueta1Y = 250;
 var raqueta2Y = 250;
 const ANCHO_RAQUETA = 10;
 const ALTURA_RAQUETA = 100;
-var velocidadRaqueta2 = 6;
+var velocidadRaqueta2 = 4;
 var raquetaOffset = 30;
 
 var puntajeJugador1 = 0;
 var puntajeJugador2 = 0;
 const PUNTAJE_GANADOR = 3;
 var mostrarPantallaFinal = false;
+var margin = 100;
 
 window.onload = function() {
 	canvas = document.getElementById('videojuego-pong');
@@ -27,6 +30,8 @@ window.onload = function() {
 		moverTodo();
 		dibujarTodo();
 	}, 1000/fps);
+
+	canvas.addEventListener('mousedown', mouseClick);
 
 	canvas.addEventListener('mousemove', function(evt) {
 		var posicionMouse = calcularPosicionDelMouse(evt);
@@ -61,10 +66,7 @@ function moverTodo() {
 			resetearBola();
 		}
 	}
-	if (bolaY < 0) {
-		velocidadBolaY = -velocidadBolaY;
-	}
-	if (bolaY > canvas.height) {
+	if (bolaY < 0 || bolaY > canvas.height) {
 		velocidadBolaY = -velocidadBolaY;
 	}
 }
@@ -74,10 +76,17 @@ function dibujarTodo() {
 	hacerCuadrado(0,0, canvas.width,canvas.height, 'black');
 
 	if (mostrarPantallaFinal) {
-		ctx.fillStyle = 'white';
-		ctx.fillText('Click Para Continuar', 100,100);
+		hacerTexto(canvas.width/4, canvas.height-margin, 'Click Para Continuar', 'white');
+
+		if (puntajeJugador1 >= PUNTAJE_GANADOR) {
+			hacerTexto(canvas.width/4, margin, 'Gana el Humano!', 'green');
+		} else if (puntajeJugador2 >= PUNTAJE_GANADOR) {
+			hacerTexto(canvas.width/4, margin, 'Gana la computadora', 'red');
+		}
 		return;
 	}
+
+	dibujarRed();
 
 	// la bola
 	hacerCirculo(bolaX,bolaY, 10, 'white');
@@ -87,8 +96,8 @@ function dibujarTodo() {
 	hacerCuadrado(canvas.width-ANCHO_RAQUETA,raqueta2Y, ANCHO_RAQUETA,ALTURA_RAQUETA, 'white');
 
 	// puntajes
-	ctx.fillText(puntajeJugador1, 100, 100);
-	ctx.fillText(puntajeJugador2, canvas.width-100, 100);
+	hacerTexto(margin, margin, puntajeJugador1, 'white');
+	hacerTexto(canvas.width-margin, margin, puntajeJugador2, 'white');
 }
 
 function hacerCuadrado(x, y, ancho, alto, color) {
@@ -102,6 +111,12 @@ function hacerCirculo(x, y, radio, color) {
 	ctx.arc(x,y, radio, 0,Math.PI*2, true);
 	ctx.fill();
 	ctx.closePath();
+}
+
+function hacerTexto(x, y, texto, color) {
+	ctx.fillStyle = color;
+	ctx.font = '20px arial'
+	ctx.fillText(texto, x, y);
 }
 
 function calcularPosicionDelMouse(evt) {
@@ -118,11 +133,10 @@ function calcularPosicionDelMouse(evt) {
 
 function resetearBola() {
 	if (puntajeJugador1 >= PUNTAJE_GANADOR || puntajeJugador2 >= PUNTAJE_GANADOR) {
-		puntajeJugador1 = 0;
-		puntajeJugador2 = 0;
 		mostrarPantallaFinal = true;
 	}
-	velocidadBolaX = -velocidadBolaX;
+	velocidadBolaX = -VELOCIDAD_INICIAL_X;
+	velocidadBolaY = VELOCIDAD_INICIAL_Y;
 	bolaX = canvas.width/2;
 	bolaY = canvas.height/2;
 }
@@ -134,5 +148,21 @@ function movimientoComputadora() {
 		raqueta2Y += velocidadRaqueta2;
 	} else if (centroRaqueta > bolaY+raquetaOffset) {
 		raqueta2Y -= velocidadRaqueta2;
+	}
+}
+
+function mouseClick() {
+	if (mostrarPantallaFinal) {
+		puntajeJugador1 = 0;
+		puntajeJugador2 = 0;
+		mostrarPantallaFinal = false;
+	}
+}
+
+function dibujarRed() {
+	var contador = 0;
+	while (contador < canvas.height) {
+		hacerCuadrado(canvas.width/2-1, contador, 2, 20, 'grey');
+		contador += 40;
 	}
 }
