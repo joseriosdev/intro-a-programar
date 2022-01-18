23d2252aa5eb9395f6f18dd20547aadcda9fce0b
Jose Rios <joseriosdev@gmail.com>
2022-01-18 10:59:59 -0500

    2.3 Moviendo la raqueta y la bola en 2d

--- pong.js
+++ pong.js
@@ -1,7 +1,14 @@
 var canvas;
 var ctx;
+
 var bolaX = 50;
+var bolaY = 50;
 var velocidadBolaX = 10;
+var velocidadBolaY = 5;
+
+var raqueta1Y = 250;
+const ANCHO_RAQUETA = 10;
+const ALTURA_RAQUETA = 100;
 
 window.onload = function() {
 	canvas = document.getElementById('videojuego-pong');
@@ -12,16 +19,28 @@ window.onload = function() {
 		moverTodo();
 		dibujarTodo();
 	}, 1000/fps);
+
+	canvas.addEventListener('mousemove', function(evt) {
+		var posicionMouse = calcularPosicionDelMouse(evt);
+		raqueta1Y = posicionMouse.y - (ALTURA_RAQUETA/2);
+	});
 }
 
 function moverTodo() {
 	bolaX += velocidadBolaX;
+	bolaY += velocidadBolaY;
 
 	if (bolaX < 0) {
 		velocidadBolaX = -velocidadBolaX;
 	}
 	if (bolaX > canvas.width) {
 		velocidadBolaX = -velocidadBolaX;
+	}
+	if (bolaY < 0) {
+		velocidadBolaY = -velocidadBolaY;
+	}
+	if (bolaY > canvas.height) {
+		velocidadBolaY = -velocidadBolaY;
 	}
 }
 
@@ -30,10 +49,10 @@ function dibujarTodo() {
 	hacerCuadrado(0,0, canvas.width,canvas.height, 'black');
 
 	// la bola
-	hacerCirculo(bolaX,200, 10, 'white');
+	hacerCirculo(bolaX,bolaY, 10, 'white');
 
 	// raqueta izquierda
-	hacerCuadrado(10,100, 10,100, 'white');
+	hacerCuadrado(10,raqueta1Y, ANCHO_RAQUETA,ALTURA_RAQUETA, 'white');
 }
 
 function hacerCuadrado(x, y, ancho, alto, color) {
@@ -47,4 +66,16 @@ function hacerCirculo(x, y, radio, color) {
 	ctx.arc(x,y, radio, 0,Math.PI*2, true);
 	ctx.fill();
 	ctx.closePath();
+}
+
+function calcularPosicionDelMouse(evt) {
+	var coordenadasCanvas = canvas.getBoundingClientRect();
+	var raiz = document.documentElement;
+	var mouseX = evt.clientX - coordenadasCanvas.left - raiz.scrollLeft;
+	var mouseY = evt.clientY - coordenadasCanvas.top - raiz.scrollTop;
+
+	return {
+		x: mouseX,
+		y: mouseY
+	};
 }
