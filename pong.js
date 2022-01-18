52a7f5febd7431d673146945cc4c966421bb359c
Jose Rios <joseriosdev@gmail.com>
2022-01-18 09:36:45 -0500

    1.2 Movimiento y tiempo

--- pong.js
+++ pong.js
@@ -1,19 +1,18 @@
 var canvas;
 var ctx;
+var bolaX = 50;
 
 window.onload = function() {
 	canvas = document.getElementById('videojuego-pong');
 	ctx = canvas.getContext('2d');
+	setInterval(dibujarTodo, 50);
+}
+
+function dibujarTodo() {
+	bolaX += 20;
+
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0,0, 800,600);
-	ctx.fillStyle = 'green';
-	ctx.fillRect(100,200, 100,50);
-	ctx.fillStyle = 'yellow';
-	ctx.fillRect(400,300, 100,100);
-	ctx.fillStyle = 'red';
-	ctx.fillRect(canvas.width,canvas.height, 50,50);
-	ctx.fillStyle = 'red';
-	ctx.fillRect(canvas.width-50,canvas.height-50, 50,50);
-	ctx.fillStyle = 'blue';
-	ctx.fillRect(canvas.width/2-50,canvas.height/2-50, 100,100);
+	ctx.fillStyle = 'orange';
+	ctx.fillRect(bolaX,200, 50,50);
 }
